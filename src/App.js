52f2e11a548a0cbd6b15269ead81c1669c49061b
Nick-Maldonado <nickmaldonado94@gmail.com>
2018-03-16 16:15:34 -0500

    Button functionality: working.  Tested with console.log

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ class App extends Component {
     };
   }
 
+  deleteToDo(index) {
+    console.log("delete button pressed");
+  }  
+
   handleChange(e) {
     this.setState({ newTodoDescription: e.target.value })
   }
@@ -38,7 +42,7 @@ class App extends Component {
       <div className="App">
         <ul>
           { this.state.todos.map( (todo, index) => 
-            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteToDo= { () => this.deleteToDo(index) } />
           )}
         </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
